4d70f50707042bacda79d65957856ce2716d7d73
mateus-joaquim <m.joaquim.fs@gmail.com>
2024-09-18 18:11:10 -0300

    Modificacao: Melhorando código + programacao defensiva

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,23 +1,16 @@
 export class Negociacao {
   constructor(
-    private _data: Date,
-    private _quantidade: number,
-    private _valor: number
+    public readonly _data: Date,
+    public readonly quantidade: number,
+    public readonly valor: number
   ) {}
 
   get data(): Date {
-    return this._data;
-  }
-
-  get quantidade(): number {
-    return this._quantidade;
-  }
-
-  get valor(): number {
-    return this._valor;
+    const data = new Date(this._data.getTime());
+    return data;
   }
 
   get volume(): number {
-    return this._quantidade * this._valor;
+    return this.quantidade * this.valor;
   }
 }
